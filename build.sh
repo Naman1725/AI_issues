975c3bb8e468491584949e2196ed5e4ad56e9d64
Naman Sharma <sharma.minaxi@gmail.com>
2025-08-14 15:44:47 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Pre-downloading ML models..."
+python -c "
+from transformers import pipeline;
+print('Downloading classifier model...');
+pipeline('zero-shot-classification', model='typeform/distilbert-base-uncased-mnli');
+print('Downloading summarizer model...');
+pipeline('summarization', model='sshleifer/distilbart-cnn-6-6');
+"
+
+echo "Build completed!"
